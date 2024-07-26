0ac016a03178344b2c9ea662e065db41a1c8df4c
alighassabbashi2019 <alighassabbashi@gmail.com>
2024-07-26 14:28:43 +0330

    change a directory name

--- src/module/authentication/model/index.ts
+++ src/module/authentication/model/index.ts
@@ -1,9 +1,9 @@
 /* entities */
 export * from './role/role.entity';
-export * from './permission/action.entity';
+export * from '@authentication/model/action/action.entity';
 export * from './role-permission/role-permission.entity';
 
 /* repositories */
 export * from './role/role.repository';
-export * from './permission/action.repository';
+export * from '@authentication/model/action/action.repository';
 export * from './role-permission/role-permission.repository';
